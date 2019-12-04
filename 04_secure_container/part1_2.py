c6b7356facf88ae71211436e7cef55e21319c7a4
Jacob Deppen <deppen.8@gmail.com>
2019-12-04 00:32:59 -0800

    drop unnecessary code

--- 04_secure_container/part1_2.py
+++ 04_secure_container/part1_2.py
@@ -1,13 +1,7 @@
 from collections import Counter
 
-
+# Given input was "382345-843167", but the first and last actual possibilities are 388888 and 799999
 INPUT_RANGE = range(388888, 800000)
-
-
-def has_zero_one_two(num: str):
-    if any(digit in num for digit in ["0", "1", "2"]):
-        return True
-    return False
 
 
 def has_consecutive_numbers_and_increases(num: str):
@@ -21,10 +15,10 @@ def has_consecutive_numbers_and_increases(num: str):
 possibilities = []
 for n in INPUT_RANGE:
     s = str(n)
-    if not has_zero_one_two(s) and has_consecutive_numbers_and_increases(s):
+    if has_consecutive_numbers_and_increases(s):
         possibilities.append(int(s))
 
-# print(len(possibilities))
+print(len(possibilities))
 
 new_possibilities = []
 for value in possibilities:
@@ -33,3 +27,10 @@ for value in possibilities:
         new_possibilities.append(s)
 
 print(len(new_possibilities))
+
+
+# Logically this has to be True for valid numbers, but not actually needed to solve the challenge.
+# def has_zero_one_two(num: str):
+#     if any(digit in num for digit in ["0", "1", "2"]):
+#         return True
+#     return False
